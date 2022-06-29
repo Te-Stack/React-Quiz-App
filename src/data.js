97c16e1ce41154c78e6c623953db91e3758c9f99
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-06-29 17:55:25 +0100

    Working

---
+++ src/data.js
@@ -0,0 +1,333 @@
+export const data = [
+    {
+      id: 1,
+      question: "In the UK, the abbreviation NHS stands for National what Service?",
+      answers: [
+        {
+          text: "Humanity",
+          correct: false,
+        },
+        {
+          text: "Health",
+          correct: true,
+        },
+        {
+          text: "Honour",
+          correct: false,
+        },
+        {
+          text: "Household",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "Which Disney character famously leaves a glass slipper behind at a royal ball",
+      answers: [
+        {
+          text: "Pocahontas",
+          correct: false,
+        },
+        {
+          text: "Sleeping Beauty",
+          correct: false,
+        },
+        {
+          text: "Cinderella",
+          correct: true,
+        },
+        {
+          text: "Elsa",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?",
+      answers: [
+        {
+          text: "Hangar",
+          correct: false,
+        },
+        {
+          text: "Terminal",
+          correct: false,
+        },
+        {
+          text: "Concourse",
+          correct: false,
+        },
+        {
+          text: "Carousel",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Which of these brands was chiefly associated with the manufacture of household locks?",
+      answers: [
+        {
+          text: "Philips",
+          correct: false,
+        },
+        {
+          text: "Flymo",
+          correct: false,
+        },
+        {
+          text: "Chubb",
+          correct: true,
+        },
+        {
+          text: "Ronseal",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "The hammer and sickle is one of the most recognisable symbols of which political ideology?",
+      answers: [
+        {
+          text: "Republicanism",
+          correct: false,
+        },
+        {
+          text: "Communism",
+          correct: true,
+        },
+        {
+          text: "Conservatism",
+          correct: false,
+        },
+        {
+          text: "Liberalism",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Which toys have been marketed with the phrase “robots in disguise”?",
+      answers: [
+        {
+          text: "Bratz Dolls",
+          correct: false,
+        },
+        {
+          text: "Sylvanian Families",
+          correct: false,
+        },
+        {
+          text: "Hatchimals",
+          correct: false,
+        },
+        {
+          text: "Transformers",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "What does the word loquacious mean?",
+      answers: [
+        {
+          text: "Angry",
+          correct: false,
+        },
+        {
+          text: "Chatty",
+          correct: true,
+        },
+        {
+          text: "Beautiful",
+          correct: false,
+        },
+        {
+          text: "Shy",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Obstetrics is a branch of medicine particularly concerned with what?",
+      answers: [
+        {
+          text: "Childbirth",
+          correct: true,
+        },
+        {
+          text: "Broken bones",
+          correct: false,
+        },
+        {
+          text: "Heart conditions",
+          correct: false,
+        },
+        {
+          text: "Old age",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "In Doctor Who, what was the signature look of the fourth Doctor, as portrayed by Tom Baker?",
+      answers: [
+        {
+          text: "Bow-tie,braces and tweed jacket",
+          correct: false,
+        },
+        {
+          text: "Wide-brimmed hat and extra long scarf",
+          correct: true,
+        },
+        {
+          text: "Pinstripe suit and trainers",
+          correct: false,
+        },
+        {
+          text: "Cape, velvet jacket and frilly shirt",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Which of these religious observances lasts for the shortest period of time during the calendar year?",
+      answers: [
+        {
+          text: "Ramadan",
+          correct: false,
+        },
+        {
+          text: "Diwali",
+          correct: true,
+        },
+        {
+          text: "Lent",
+          correct: false,
+        },
+        {
+          text: "Hanukkah",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "At the closest point, which island group is only 50 miles south-east of the coast of Florida?",
+      answers: [
+        {
+          text: "Bahamas",
+          correct: true,
+        },
+        {
+          text: "US Virgin Islands",
+          correct: false,
+        },
+        {
+          text: "Turks and Caicos Island",
+          correct: false,
+        },
+        {
+          text: "Bermuda",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Construction of which of these famous landmarks was completed first?",
+      answers: [
+        {
+          text: "Empire State Building",
+          correct: false,
+        },
+        {
+          text: "Royal Albert Hall",
+          correct: false,
+        },
+        {
+          text: "Eiffel Tower",
+          correct: false,
+        },
+        {
+          text: "Big Ben Clock Tower",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Which of these cetaceans is classified as a “toothed whale”?",
+      answers: [
+        {
+          text: "Gray whale ",
+          correct: false,
+        },
+        {
+          text: "Minke whale",
+          correct: false,
+        },
+        {
+          text: "Sperm whale",
+          correct: true,
+        },
+        {
+          text: "Humpback whale",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Who is the only British politician to have held all four “Great Offices of State” at some point during their career?",
+      answers: [
+        {
+          text: "David Lloyd George",
+          correct: false,
+        },
+        {
+          text: "Harold Wilson",
+          correct: false,
+        },
+        {
+          text: "James Callaghan",
+          correct: true,
+        },
+        {
+          text: "John Mayor",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "In 1718, which pirate died in battle off the coast of what is now North Carolina?",
+      answers: [
+        {
+          text: "Calico Jack ",
+          correct: false,
+        },
+        {
+          text: "Blackbeard",
+          correct: true,
+        },
+        {
+          text: "Bartholomew Roberts ",
+          correct: false,
+        },
+        {
+          text: "Captain Kidd",
+          correct: false,
+        },
+      ],
+    },
+  ];
+
